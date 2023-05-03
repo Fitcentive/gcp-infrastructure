fae7d78ca06450542a2be571aedac180062826c2
Vidhyasagar Harihara <vidhyasagar1210@gmail.com>
2023-05-03 00:37:16 -0400

    fix: Add custom nginx-default-backend image as null resource and add default backend to nginx helm release

--- modules/nginx-ingress-controller/main.tf
+++ modules/nginx-ingress-controller/main.tf
@@ -4,6 +4,17 @@ resource "kubernetes_namespace" "nginx" {
   }
 }
 
+# Create and push custom nginx default backend image
+resource "null_resource" "push_custom_nginx_ingress_default_backend_image_to_gcr" {
+  provisioner "local-exec" {
+    command = <<-EOT
+      docker build -t ${var.helm_nginx_name}-default-backend:latest -t ${var.helm_nginx_name}-default-backend:1.0 ${path.module}/resources/
+      docker tag ${var.helm_nginx_name}-default-backend:1.0 gcr.io/${var.project_id}/${var.helm_nginx_name}-default-backend:1.0
+      docker push gcr.io/${var.project_id}/${var.helm_nginx_name}-default-backend:1.0
+    EOT
+  }
+}
+
 resource "helm_release" "nginx" {
   repository = "https://kubernetes.github.io/ingress-nginx"
   chart      = "ingress-nginx"
@@ -13,11 +24,14 @@ resource "helm_release" "nginx" {
 
   values = [
     sensitive(templatefile("${path.module}/resources/custom_NginxIngressController.yaml", {
-      STATIC_LOAD_BALANCER_IP_ADDRESS = var.regional_static_ip_address
+      STATIC_LOAD_BALANCER_IP_ADDRESS = var.regional_static_ip_address,
+      GCP_PROJECT_ID = var.project_id,
+      HELM_NAME = var.helm_nginx_name,
     }))
   ]
 
   depends_on = [
-    kubernetes_namespace.nginx
+    kubernetes_namespace.nginx,
+    null_resource.push_custom_nginx_ingress_default_backend_image_to_gcr
   ]
 }
