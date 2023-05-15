4178de265d5dbe0d9e1ae48d45736bae2555c9bf
Vidhyasagar Harihara <vidhyasagar1210@gmail.com>
2023-05-15 11:21:27 -0400

    feat: Add cloudsql db to public-gateway service module

--- gke-dev-env-02/terraform/main.tf
+++ gke-dev-env-02/terraform/main.tf
@@ -291,6 +291,10 @@ module "dev-public-gateway-service" {
   ad_unit_id_android = local.ad_unit_id_android
   ad_unit_id_ios     = local.ad_unit_id_ios
 
+  cloud_sql_instance_connection_name = module.cloudsql-dev-env.cloudsql_instance_connection_name
+  cloud_sql_instance_name            = module.cloudsql-dev-env.cloudsql_instance_name
+  cloudsql_service_account_key       = module.cloudsql-dev-env.cloudsql_service_account_key
+
   depends_on = [
     module.gke-dev-functional-namespaces,
     module.cloudsql-dev-env,
--- modules/core-services/public-gateway-service/variables.tf
+++ modules/core-services/public-gateway-service/variables.tf
@@ -19,3 +19,20 @@ variable "ad_unit_id_android" {
 variable "ad_unit_id_ios" {
   description = "Google AdMob iOS Ad Unit ID. Available via AdMob UI"
 }
+
+variable "cloud_sql_instance_name" {
+  description = "GCP Cloud SQL instance to create databases on"
+}
+
+variable "cloud_sql_instance_connection_name" {
+  description = "GCP Cloud SQL instance connection name"
+}
+
+variable "cloudsql_service_account_key" {
+  description = "Base64 version of service account key file for CloudSql"
+}
+
+variable "database_name" {
+  type    = string
+  default = "public-gateway-service-db"
+}
