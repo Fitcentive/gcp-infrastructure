8fab1b7f310223c73b9575c93d4d7292a3be2f74
Vidhyasagar Harihara <vidhyasagar1210@gmail.com>
2023-08-22 14:04:28 -0400

    feat: Update keycloak resource data to latest export

--- scripts/scale_up_nodes.sh
+++ scripts/scale_up_nodes.sh
@@ -2,5 +2,5 @@
 
 # Note - Might need to re-apply nginx controller to enable load balancing via static IP
 
-echo "y" | gcloud container clusters resize fitcentive-dev-03-gke --node-pool fitcentive-dev-03-gke --zone northamerica-northeast2-a --num-nodes 2
+echo "y" | gcloud container clusters resize fitcentive-dev-03-gke --node-pool fitcentive-dev-03-gke --zone northamerica-northeast2-a --num-nodes 1
 # gcloud sql instances patch gke-dev-env-cloud-sql-instance --activation-policy=ALWAYS
